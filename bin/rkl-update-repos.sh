2b332c585f09550be0402e1289cd1d521c258b3e
rkl <richard@uzori.com>
2021-10-30 12:46:12 +0200

    Added script to update GIT repos

---
+++ bin/rkl-update-repos.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#############################################################################
+##
+##  simple bash script to pull all my git repos
+##
+#############################################################################
+
+
+# ---------------------------------------------------------------------------
+#  create a list of git repos in all sub directories
+# ---------------------------------------------------------------------------
+
+REPOS=$(find . -name .git)
+
+
+# ---------------------------------------------------------------------------
+#  update all repos
+# ---------------------------------------------------------------------------
+
+for REPO in $REPOS; do
+    # since we end up in the directory .git, we need to go up one level
+    cd $REPO/..
+    PWD=$(pwd)
+    echo "Updating: $PWD ..."
+    git pull
+    # go back to where we came from
+    cd - > /dev/null
+    echo ""
+done
+
+echo "All done!"
+
+
+### eof #####################################################################
